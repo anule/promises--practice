e26a684b162314dda5d56c7d371d8f71fc189b24
Anule Ndukwu <anule.ndukwu@gmail.com>
2017-09-22 11:56:39 -0400

    resolve merge conflict

--- exercise-one.js
+++ exercise-one.js
@@ -44,13 +44,21 @@ function problemA () {
    */
 
   // callback version
-  readFile('poem-one/stanza-01.txt', function (err, stanza) {
-    console.log('-- A. callback version --');
-    blue(stanza);
-  });
+  // readFile('poem-one/stanza-01.txt', function (err, stanza) {
+  //   console.log('-- A. callback version --');
+  //   blue(stanza);
+  // });
 
   // promise version
-  // ???
+  promisifiedReadFile('‘poem-one/stanza-01.txt’').
+    then(function success(stanza){
+      console.log('-- A. callback version --');
+      blue(stanza);
+    }).
+    then(function error(err)
+    {
+      console.log('some issue'+err);
+    });
 
 }
 
